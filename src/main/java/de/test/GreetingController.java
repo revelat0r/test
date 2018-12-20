179dd74f6983c7b6e106b5e09bc8a14652edb956
stephan <stephan@stephans-mbp-2.local>
2018-12-20 21:39:07 +0100

    log included

--- src/main/java/de/test/GreetingController.java
+++ src/main/java/de/test/GreetingController.java
@@ -4,6 +4,8 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class GreetingController {
@@ -11,9 +13,14 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    private Logger LOG = LoggerFactory.getLogger(GreetingController.class);
+   
+    
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
+        LOG.info("Hello");
+    	
+    	return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
 }
